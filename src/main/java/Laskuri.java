c488b548421d8ca21377866c10cd5059ed8c1f4c
tomihaapala <48236861+tomihaapala@users.noreply.github.com>
2020-10-08 10:18:55 +0300

    commit 2

--- src/main/java/Laskuri.java
+++ src/main/java/Laskuri.java
@@ -8,13 +8,14 @@ public class Laskuri{
 		if(alkuarvo < 0)
 			return;
 		this.tapahtumia = alkuarvo;
+	
+
 	}
 	public Laskuri(){
 		this.tapahtumia = 0;
 	}
-
-  	public void lisaa(){
-    		this.tapahtumia = this.tapahtumia + 1; 
+  	public void lisaa(int lisays){
+    		this.tapahtumia = this.tapahtumia + lisays;         
   	}
   	public int lukema(){
     		return this.tapahtumia;
@@ -23,7 +24,7 @@ public class Laskuri{
     		this.tapahtumia = 0;
   	}
 	public String toString(){
-		return "Laskurin lukema on " + this.tapahtumia;
+		return "Lukema " + this.tapahtumia;
 	}
 
 }
